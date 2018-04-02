c8b11e6eb23b3d86fe74a270f8e68a24569424f1
Vladimir <dosandkv@gmail.com>
2018-04-03 00:56:17 +0300

    Included reports app to core-app;

--- src/app/reports/reports.module.ts
+++ src/app/reports/reports.module.ts
@@ -1,15 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+// import { RouterModule } from '@angular/router';
 
 import { ReportsComponent } from './reports.component';
+import { ReportsListComponent } from './reports-list/reports-list.component';
+import { ReportDetailsComponent } from './report-details/report-details.component';
 
 
 @NgModule({
   declarations: [
     ReportsComponent,
+    ReportsListComponent,
+    ReportDetailsComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    // RouterModule.forRoot(
+    //   [
+    //     { path: 'reports-list', component: ReportsListComponent },
+    //     { path: 'report-details', component: ReportDetailsComponent}
+    //   ]
+    // )
   ],
   bootstrap: [ReportsComponent],
   providers: [],
